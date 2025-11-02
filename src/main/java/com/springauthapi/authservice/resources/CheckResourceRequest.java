8c31b26bb4ca0bb388f243f30f9d90be69853e45
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-11-02 20:26:13 +0000

    update the CheckResourceRequest to ignore the getSafeResourceId method in the JOSN request

--- src/main/java/com/springauthapi/authservice/resources/CheckResourceRequest.java
+++ src/main/java/com/springauthapi/authservice/resources/CheckResourceRequest.java
@@ -2,6 +2,8 @@ package com.springauthapi.authservice.resources;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -17,8 +19,10 @@ public class CheckResourceRequest {
     @Size(min = 1, message = "At least one action type must be provided")
     private List<String> actionTypes;
 
+
     private String resourceId;
 
+    @JsonIgnore
     public String getSafeResourceId() {
         return (resourceId == null || resourceId.isBlank()) ? "*" : resourceId;
     }
