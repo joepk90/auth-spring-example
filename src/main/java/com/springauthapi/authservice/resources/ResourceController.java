e9a8e750d203ba488b36b00e0d9c46e4ab06e393
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-11-02 20:24:47 +0000

    update the ResourceController root path name: resources -> resource + name it using the swagger Tag annotation

--- src/main/java/com/springauthapi/authservice/resources/ResourceController.java
+++ src/main/java/com/springauthapi/authservice/resources/ResourceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.springauthapi.authservice.policies.PolicyServiceResponseDto;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -16,7 +17,8 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/resources")
+@Tag(name = "Resource Controller")
+@RequestMapping("/resource")
 public class ResourceController {
 
     private final ResearceService researceService;
